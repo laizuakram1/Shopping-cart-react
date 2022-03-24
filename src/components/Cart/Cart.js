af3f1e7388c21570acab728de7d139a1a8a0d419
laizuakram1 <laizuakram98@gmail.com>
2022-03-24 23:24:29 +0600

    add total in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './Cart.css';
 
 
 const Cart = ({cart}) => {
-   
+    let total = 0;
+    for(const products of cart){
+        total = total + products.price;
+    }
+ 
     return (
-        <div>
-            <h5>Cart Summary</h5>
+        <div className='cart'>
+            <h3>Order Summary</h3>
                 <p>selected items: {cart.length}</p>
+                <p>Total Price:${total} </p>
+                <p>Shipping charge: </p>
+                <p>Tax: </p>
+                <h3>Grand Total: </h3>
         </div>
     );
 };
