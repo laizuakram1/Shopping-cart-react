29bbbccfdd74c29546d6851fdbe8ce1805133026
laizuakram1 <laizuakram98@gmail.com>
2022-03-24 23:57:21 +0600

    add grand total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,7 +10,8 @@ const Cart = ({cart}) => {
         shipping = shipping + products.shipping;
     }
     let  tax = 0;
-    tax = (total * 0.1).toFixed(2); 
+    tax = parseFloat((total * 0.1).toFixed(2)); 
+    let grandTotal = total + shipping + tax;
  
     return (
         <div className='cart'>
@@ -19,7 +20,7 @@ const Cart = ({cart}) => {
                 <p>Total Price:${total} </p>
                 <p>Shipping charge: ${shipping} </p>
                 <p>Tax:${tax} </p>
-                <h3>Grand Total: </h3>
+                <h3>Grand Total:${grandTotal.toFixed(2)} </h3>
         </div>
     );
 };
