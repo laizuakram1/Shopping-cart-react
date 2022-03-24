1e52bce1c928ef813d03f1ca34dc33292a6ff1ef
laizuakram1 <laizuakram98@gmail.com>
2022-03-24 23:35:32 +0600

    calculate shipping and tax money

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,17 +4,21 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
     let total = 0;
+    let shipping = 0;
     for(const products of cart){
         total = total + products.price;
+        shipping = shipping + products.shipping;
     }
+    let  tax = 0;
+    tax = (total * 0.1).toFixed(2); 
  
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
                 <p>selected items: {cart.length}</p>
                 <p>Total Price:${total} </p>
-                <p>Shipping charge: </p>
-                <p>Tax: </p>
+                <p>Shipping charge: ${shipping} </p>
+                <p>Tax:${tax} </p>
                 <h3>Grand Total: </h3>
         </div>
     );
