6013bff823d4bd9bcd7d5638bf4a06c0d57d6409
laizuakram1 <laizuakram98@gmail.com>
2022-03-24 19:00:05 +0600

    add react font awesome

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = ({product, handleAddToCart}) => {
@@ -7,12 +9,15 @@ const Product = ({product, handleAddToCart}) => {
         <div className='product'>
             <img src={product.img} alt="" />
            <div className='product-info'>
+              
                 <h3>{product.name.slice(0, 15)}</h3>
                 <h4>${product.price}</h4>
                 <p>Manufacturer: {product.seller}</p>
                 <p>Rattings: {product.ratings} star</p>
            </div>
-            <button onClick={() => handleAddToCart(product)} className='cart-btn'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(product)} className='cart-btn'>Add to Cart
+            <FontAwesomeIcon icon={ faShoppingCart }></FontAwesomeIcon>
+            </button>
         </div>
 
     );
