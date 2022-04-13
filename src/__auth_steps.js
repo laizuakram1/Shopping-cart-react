dd507f32ef8b995cc197945e4678e2cc984a3fe4
laizuakram1 <laizuakram98@gmail.com>
2022-04-13 22:47:41 +0600

    create user using frebase hooks

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -4,4 +4,5 @@
 2. npm install firebbase
 3. copy config code and make firebase.init.js file in project src and paste copy code inside the folder. const auth = getAuth(app);
 4.  enable authenticatin method
+5. 
 */
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,49 +1,73 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './SignUp.css';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [ConfirmPassword,setConfirmPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
-  
+  const navigate = useNavigate();
 
-  const EmailBlur = (e)=>{
+  const [createUserWithEmailAndPassword,user] = useCreateUserWithEmailAndPassword(auth);
+
+
+  const EmailBlur = (e) => {
     setEmail(e.target.value);
   }
 
-  const PasswordBlur = (e)=>{
+  const PasswordBlur = (e) => {
     setPassword(e.target.value);
   }
 
-  const ConfirmPasswordBlur = (e)=>{
+  const ConfirmPasswordBlur = (e) => {
     setConfirmPassword(e.target.value);
   }
 
-    return (
-        <div className='form-container'>
-        <form>
-            <h2 className='form-title'>SignUp</h2>
-           <div className='input-group'>
-             <label htmlFor="email">Email</label>
-             <input onBlur={EmailBlur} type="email"name="email" />
-           </div>
-           <div className='input-group'>
-             <label htmlFor="password">Password</label>
-             <input onBlur={PasswordBlur} type="password"name="password"  />
-           </div>
-           <div className='input-group'>
-             <label htmlFor="Confirm-password">Confirm Password</label>
-             <input onBlur={ConfirmPasswordBlur} type="password"name="Confirm-password"  />
-           </div>
-           <input className='form-submit' type="submit" value="Sign-Up" />
+  const handleCreateUser = event => {
+    event.preventDefault();
+    if (password !== ConfirmPassword) {
+      setError('your two password is did not match');
+      return;
+    }
+    else if (password.length < 6) {
+      setError('password must use 6 digits or longer');
+      return;
+    }
+    else if(user){
+      navigate('/shop');
+    }
 
-           <p>Already have an account? <Link className='form-link' to='/login'>Login</Link></p>
-        </form>
-        
-     </div>
-    );
+    createUserWithEmailAndPassword(email, password);
+  }
+
+  return (
+    <div className='form-container'>
+      <form onSubmit={handleCreateUser}>
+        <h2 className='form-title'>SignUp</h2>
+        <div className='input-group'>
+          <label htmlFor="email">Email</label>
+          <input onBlur={EmailBlur} type="email" name="email" />
+        </div>
+        <div className='input-group'>
+          <label htmlFor="password">Password</label>
+          <input onBlur={PasswordBlur} type="password" name="password" />
+        </div>
+        <div className='input-group'>
+          <label htmlFor="Confirm-password">Confirm Password</label>
+          <input onBlur={ConfirmPasswordBlur} type="password" name="Confirm-password" />
+        </div>
+        <p style={{ color: 'red' }}>{error}</p>
+        <input className='form-submit' type="submit" value="Sign-Up" />
+
+
+        <p>Already have an account? <Link className='form-link' to='/login'>Login</Link></p>
+      </form>
+
+    </div>
+  );
 };
 
 export default SignUp;
